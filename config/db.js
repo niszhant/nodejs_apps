6aac760ae9cd79c1dc5fa26d92d61946599df10a
stgr99 <nishantkumar988@gmail.com>
2018-05-10 18:16:26 +0530

    removed mongodb password

--- config/db.js
+++ config/db.js
@@ -3,16 +3,16 @@ require("../models/task");
 const mongoose = require('mongoose');
 
 // DB credentials
-// stgr99@A8FKsyESd3cIdUI8
+// stgr99@<password>
 // primary : cluster0-shard-00-00-iawj0.mongodb.net:27017
 // secondary : cluster0-shard-00-01-iawj0.mongodb.net:27017
 // secondary : cluster0-shard-00-02-iawj0.mongodb.net:27017
 
 // latest driver 3.6 and above
-// const uri = 'mongodb+srv://stgr99:A8FKsyESd3cIdUI8@cluster0-iawj0.mongodb.net/test?retryWrites=true';
+// const uri = 'mongodb+srv://stgr99:<password>@cluster0-iawj0.mongodb.net/test?retryWrites=true';
 
 // Driver 3.4 or earlier
-const dbURI = 'mongodb://stgr99:A8FKsyESd3cIdUI8@cluster0-shard-00-00-iawj0.mongodb.net:27017,cluster0-shard-00-01-iawj0.mongodb.net:27017,cluster0-shard-00-02-iawj0.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+const dbURI = 'mongodb://stgr99:<password>@cluster0-shard-00-00-iawj0.mongodb.net:27017,cluster0-shard-00-01-iawj0.mongodb.net:27017,cluster0-shard-00-02-iawj0.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 const options = {
     reconnectTries: Number.MAX_VALUE,
     poolSize: 10
